f5b1b3093027a3edd4429bdcc965f3cd48b0be62
FBagdeli <f.bagdeli13@outlook.com>
2023-10-09 19:19:51 +0200

    MainPagev01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.farshadchalenges.bazimizi"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.farshadchalenges.bazimizi"
@@ -31,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -52,7 +52,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -67,4 +67,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.room:room-ktx:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
 }
---
+++ app/src/main/java/com/farshadchalenges/bazimizi/MainPage.kt
@@ -0,0 +1,178 @@
+package com.farshadchalenges.bazimizi
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.system.exitProcess
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainPage() {
+
+    val exitDialog = remember {
+        mutableStateOf(false)
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = stringResource(id = R.string.app_name)
+                        )
+                        Text(text = stringResource(id = R.string.app_name))
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_500),
+                    titleContentColor = Color.White
+                )
+            )
+
+        }
+    ) { paddingValues ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Spacer(modifier = Modifier.height(150.dp))
+
+            Button(
+                onClick = {
+                    //new Game
+                }, colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.purple_500),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(
+                    2.dp, colorResource(id = R.color.purple_700)
+                ),
+                modifier = Modifier.size(200.dp, 60.dp)
+            ) {
+                Text(text = stringResource(id = R.string.new_game), fontSize = 20.sp)
+            }
+
+            Button(
+                onClick = {
+                    //History
+                }, colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.purple_500),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(
+                    2.dp, colorResource(id = R.color.purple_700)
+                ),
+                modifier = Modifier.size(200.dp, 60.dp)
+            ) {
+                Text(text = stringResource(id = R.string.history), fontSize = 20.sp)
+            }
+            Button(
+                onClick = {
+                    //Settings
+                }, colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.purple_500),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(
+                    2.dp, colorResource(id = R.color.purple_700)
+                ),
+                modifier = Modifier.size(200.dp, 60.dp)
+            ) {
+                Text(text = stringResource(id = R.string.setting), fontSize = 20.sp)
+            }
+            Button(
+                onClick = {
+                    exitDialog.value = true
+
+                }, colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.purple_500),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(
+                    2.dp, colorResource(id = R.color.purple_700)
+                ),
+                modifier = Modifier.size(200.dp, 60.dp)
+            ) {
+                Text(text = stringResource(id = R.string.exit), fontSize = 20.sp)
+            }
+        }
+        if (exitDialog.value) {
+            AlertDialog(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Warning,
+                        contentDescription = "",
+                        tint = colorResource(
+                            id = R.color.yellow
+                        )
+                    )
+                },
+                onDismissRequest = {
+                    exitDialog.value = false
+                },
+                confirmButton = {
+                    Button(onClick = {
+                        exitProcess(0)
+                    }) {
+                        Text(text = stringResource(id = R.string.yes))
+                    }
+                }, dismissButton = {
+                    Button(onClick = {
+                        exitDialog.value = false
+                    }) {
+                        Text(text = stringResource(id = R.string.no))
+                    }
+                },
+                text = {
+                    Text(text = stringResource(id = R.string.do_you_want_to_exit), fontSize = 20.sp)
+                },
+                titleContentColor = Color.Black,
+            )
+        }
+    }
+}
