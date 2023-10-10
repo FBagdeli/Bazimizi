b9987fcf9f8c9bf1cfc42c6a75764b288e5bbc44
FBagdeli <f.bagdeli13@outlook.com>
2023-10-10 10:02:51 +0200

    exit Dialog Finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+
     }
 
     buildTypes {
@@ -51,6 +52,7 @@ android {
 }
 
 dependencies {
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
--- app/src/main/java/com/farshadchalenges/bazimizi/MainPage.kt
+++ app/src/main/java/com/farshadchalenges/bazimizi/MainPage.kt
@@ -1,5 +1,6 @@
 package com.farshadchalenges.bazimizi
 
+import android.app.AlertDialog
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -21,6 +23,7 @@ import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -34,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -41,6 +45,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 import kotlin.system.exitProcess
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -150,7 +155,7 @@ fun MainPage() {
             AlertDialog(
                 icon = {
                     Image(
-                        painter = painterResource(id = R.drawable.yellow_warning_icon),
+                        painter = painterResource(id = R.drawable.warning_icon),
                         contentDescription = "",
                         modifier = Modifier.size(70.dp)
                     )
@@ -159,39 +164,51 @@ fun MainPage() {
                     exitDialog.value = false
                 },
                 text = {
-                    Text(text = stringResource(id = R.string.do_you_want_to_exit), fontSize = 20.sp, modifier = Modifier.fillMaxWidth(),textAlign = TextAlign.Center)
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.do_you_want_to_exit),
+                            fontSize = 20.sp,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            color = Color.Black
+                        )
+                        Spacer(modifier = Modifier.height(30.dp))
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Button(
+                                onClick = {
+                                    exitDialog.value = false
+                                },
+                                modifier = Modifier.size(100.dp, 40.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = colorResource(id = R.color.lightGray)
+                                ),
+                                border = BorderStroke(1.dp, color = Color.Black)
+                            ) {
+                                Text(text = stringResource(id = R.string.no))
+                            }
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Button(
+                                onClick = {
+                                    exitProcess(0)
+                                }, modifier = Modifier.size(100.dp, 40.dp),
+                                border = BorderStroke(1.dp, color = Color.Black)
+                            ) {
+                                Text(text = stringResource(id = R.string.yes))
+                            }
+                        }
+                    }
+
+
                 },
-
-                confirmButton = {
-                    Row(modifier = Modifier.width(200.dp)) {
-                        Button(
-                            onClick = {
-                                exitProcess(0)
-                            },
-                            border = BorderStroke(1.dp, color = Color.Black)
-                        ) {
-                            Text(text = stringResource(id = R.string.yes))
-                        }
-                    }
-
-
-                }, dismissButton = {
-                    Row() {
-                        Button(
-                            onClick = {
-                                exitDialog.value = false
-
-
-                            }, colors = ButtonDefaults.buttonColors(
-                                containerColor = colorResource(id = R.color.lightGray)
-                            ), border = BorderStroke(1.dp, color = Color.Black)
-                        ) {
-                            Text(text = stringResource(id = R.string.no))
-                        }
-                    }
-
-                }
-
+                containerColor = colorResource(id = R.color.purple_200),
+                confirmButton = {}
             )
         }
     }
